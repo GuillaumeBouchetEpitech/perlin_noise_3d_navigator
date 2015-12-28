64c6f69978a98d146ae682f1e701bebde0b9c957
GuillaumeBouchetEpitech <guillaume.bouchet@epitech.eu>
2015-12-28 09:57:15 +0000

    cleaned the main

--- src/main.cpp
+++ src/main.cpp
@@ -3,49 +3,6 @@
 
 #include <QApplication>
 
-/**
-int main(int argc, char *argv[])
-{
-    QApplication    a(argc, argv);
-
-    Perlin3DViewerWidget    w;
-    w.show();
-
-    return (a.exec());
-}
-
-/**
-
-#include <QTabWidget>
-
-#include "widgets/chunkpropertieswidget.h"
-
-#include "navigator_globalvalue.h"
-
-
-int main(int argc, char *argv[])
-{
-    QApplication    app(argc, argv);
-
-    Navigator_GlobalValue::pTest = new Navigator_GlobalValue;
-
-    QTabWidget      w;
-    w.setMinimumSize(500, 400);
-
-    Perlin3DViewerWidget*   pChild1 = new Perlin3DViewerWidget;
-    ChunkPropertiesWidget*  pChild2 = new ChunkPropertiesWidget;
-
-    w.addTab( pChild1, QString("Child1"));
-    w.addTab( pChild2, QString("Child2"));
-
-    w.show();
-
-    return (app.exec());
-}
-
-//*/
-
-
 #include "widgets/mainwindow.h"
 
 
